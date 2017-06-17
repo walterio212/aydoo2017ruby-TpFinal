70bd882538d651a4ebcf4845cc737fa8d268a580
walterio212 <walter.dav.093@gmail.com>
2017-06-17 20:34:50 -0300

    tests persistor

--- Calendario/spec/persistor_spec.rb
+++ Calendario/spec/persistor_spec.rb
@@ -2,6 +2,8 @@ require 'rspec'
 require_relative '../model/calendario'
 require_relative '../model/persistor'
 require_relative '../model/GeneralError'
+require_relative '../model/evento_diario'
+require_relative '../model/evento_semanal'
 
 describe 'Persistor' do 
 
@@ -75,5 +77,60 @@ describe 'Persistor' do
     persistidor = Persistor.new(fileDouble, dirDouble)
 
     persistidor.borrar_calendario("calendario")
+  end
+
+  it 'Obtener calendario por nombre devuelve el objeto calendario' do 
+    fileDouble = double('File', :directory? => true, :file? => true)
+    conversorJsonObjetoDouble = double('conversorJsonObjeto')
+    dirDouble = double('Dir')
+    archivo = StringIO.new("{'nombre': 'calendario1'}\n{'nombre': 'evento1', 'calendario': 'calendario1'}")
+
+    allow(fileDouble)
+      .to receive(:open)
+      .with("almacenamientoCalendario/calendario1.txt")
+      .and_yield(archivo)
+
+    allow(fileDouble)
+      .to receive(:join)
+      .with("almacenamientoCalendario", "calendario1.txt") {"almacenamientoCalendario/calendario1.txt"} 
+
+    allow(conversorJsonObjetoDouble)
+      .to receive(:convertir_calendario_no_array)
+      .with("{'nombre': 'calendario1'}\n") {Calendario.new("calendario1")}
+
+    persistidor = Persistor.new(fileDouble, dirDouble, nil, conversorJsonObjetoDouble)
+    calendario = persistidor.obtener_calendario_por_nombre("calendario1")
+
+    expect(calendario.getNombre()).to eq "calendario1"
+  end
+
+  it 'Existe calendario devuelve true si existe el archivo de calendario' do 
+    fileDouble = double('File', :directory? => true, :file? => false)
+    dirDouble = double('Dir')
+    allow(fileDouble)
+      .to receive(:join)
+      .with("almacenamientoCalendario", "calendario1.txt") {"almacenamientoCalendario/calendario1.txt"} 
+
+    allow(fileDouble).to receive(:file?).with("almacenamientoCalendario/calendario1.txt") { true }
+
+    persistidor = Persistor.new(fileDouble, dirDouble, nil, nil)
+    existeCalendario = persistidor.existe_calendario?("calendario1")
+
+    expect(existeCalendario).to eq true
+  end
+
+  it 'Existe calendario devuelve false si no existe el archivo de calendario' do 
+    fileDouble = double('File', :directory? => true, :file? => true)
+    dirDouble = double('Dir')
+    allow(fileDouble)
+      .to receive(:join)
+      .with("almacenamientoCalendario", "calendario1.txt") {"almacenamientoCalendario/calendario1.txt"} 
+
+    allow(fileDouble).to receive(:file?).with("almacenamientoCalendario/calendario1.txt") { false }
+
+    persistidor = Persistor.new(fileDouble, dirDouble, nil, nil)
+    existeCalendario = persistidor.existe_calendario?("calendario1")
+
+    expect(existeCalendario).to eq false
   end
 end
